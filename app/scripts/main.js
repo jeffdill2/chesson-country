45a944ce76faa59e5d78230764d147837d98d83a
Jeffrey Dill <jeffdill2@gmail.com>
2014-11-03 21:34:23 -0500

    Initialize Parse and application

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,29 @@
 'use strict';
 
 //
+// INITIALIZE PARSE
+// ----------------
+Parse.initialize('Jhy8OKg078ZS0kqcovgTUTOWGVR24uLBaAbgVWr1','5YB6DJc5NNwAbp73polr1FpMRmO37VDdgaJDgGWt');
+
+//
 // INITIALIZE ROUTER
 // -----------------
 var router = new Router();
 Parse.history.start();
 
+//
+// INITIALIZE APPLICATION
+// ----------------------
+$(document).ready(function() {
+	$('#header-login-button').on('click', function() {
+		if (Parse.User.current) {
+			router.navigate('/#admin');
+		} else {
+			router.navigate('/#login');
+		}
+	});
+
+	$('.logo').on('click', function() {
+		router.navigate('/#');
+	});
+});
