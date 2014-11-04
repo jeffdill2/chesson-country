e536c09e7735a07f11a09b51411ca4cf1eb5f962
Jeffrey Dill <jeffdill2@gmail.com>
2014-11-03 21:41:42 -0500

    Create Login view

---
+++ app/scripts/login-view.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var LoginView = Parse.View.extend({
+	className: "login-view-container",
+
+	events: {
+		'click #login-button'	: 'login'
+	},
+
+	template: _.template($('#login-view-template').text()),
+
+	initialize: function(options) {
+		$('.view-container').html(this.el);
+		this.render();
+	},
+
+	render: function() {
+		var renderedTemplate = this.template;
+		this.$el.html(renderedTemplate);
+	},
+
+	login: function() {
+		var username = $('#username-input-box').val();
+		var password = $('#password-input-box').val();
+
+		if (username.length > 0 && password.length > 0 && Parse.User.current() === null) {
+			Parse.User.logIn(username, password, {
+				success: function() {
+					console.log('SUCCESS');
+					router.navigate('/#admin');
+				},
+				error: function(error) {
+					console.log('ERROR:', error);
+					// USERNAME OR PASSWORD INCORRECT SWEET ALERT HERE!!!
+				}
+			});
+		} else {
+			console.log('ENTER A USERNAME AND PASSWORD');
+			// ENTER A USERNAME AND PASSWORD SWEET ALERT HERE!!!
+		}
+	}
+});
