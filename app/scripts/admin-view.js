e4a07d54095088f9c94554552ad953d7166e9236
Jeffrey Dill <jeffdill2@gmail.com>
2014-11-16 21:49:29 -0500

    Add functionality for managing current products

--- app/scripts/admin-view.js
+++ app/scripts/admin-view.js
@@ -8,6 +8,10 @@ var AdminView = Parse.View.extend({
 		'click #add-new-product' 				: 'addNewProduct',
 		'click #add-new-item' 	 				: 'addNewItem',
 		'click #upload-image'						: 'uploadImage',
+		'click .delete-product'					: 'deleteProduct',
+		'click .edit-product' 					: 'editProduct',
+		'click .update-product' 				: 'updateProduct',
+		'click .cancel-product-update'  : 'resetProductButtons',
 		'keypress #product-name' 				: 'productKeypress',
 		'keypress #product-description' : 'productKeypress',
 		'keypress #item-name'						: 'itemKeypress',
@@ -116,7 +120,7 @@ var AdminView = Parse.View.extend({
 				$('#add-new-item').text('Add New Item');
 				$('#add-new-item').attr('disabled', false);
 			},
-			error: function() {
+			error: function(error) {
 				sweetAlert('Oops!', 'New item was unable to be created', 'error');
 
 				console.log('Error ' + error.code + " : " + error.message);
@@ -139,5 +143,105 @@ var AdminView = Parse.View.extend({
 
 	uploadImage: function() {
 		$('#image-file-selector').click();
+	},
+
+	deleteProduct: function(click) {
+		var productId = click.target.dataset.productId;
+		var query = new Parse.Query('Product');
+
+		query.get(productId, {
+			success: function(product) {
+				sweetAlert({
+					title: "Are you sure?",
+					text: "You will not be able to recover this product!",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonColor: "#DD6B55",
+					confirmButtonText: "Yes, delete it!",
+					closeOnConfirm: false
+				}, function() {
+					product.destroy();
+					$('#delete-product-' + productId).parents('tr').remove();
+
+					swal("Deleted!", "This product has been successfully removed.", "success");
+				});
+			},
+			error: function(error) {
+				console.log(error);
+			}
+		});
+	},
+
+	editProduct: function(click) {
+		var productId = click.target.dataset.productId;
+		var saveGlyphicon = '&nbsp;&nbsp;<span class="glyphicon glyphicon-floppy-disk"></span>';
+		var cancelGlyphicon = '&nbsp;&nbsp;<span class="glyphicon glyphicon-ban-circle"></span>';
+		var $nameElement = $($('#edit-product-' + productId).parents('tr').children('td')[0]);
+		var $descriptionElement = $($('#edit-product-' + productId).parents('tr').children('td')[1]);
+
+		$nameElement.html('<input id="edit-product-name" type="text" value="' + $nameElement.text() + '" data-original-value="' + $nameElement.text() + '">');
+		$descriptionElement.html('<input id="edit-product-description" type="text" value="' + $descriptionElement.text() + '" data-original-value="' + $descriptionElement.text() + '">');
+
+		$('#edit-product-' + productId).removeClass('edit-product');
+		$('#edit-product-' + productId).removeClass('btn-info');
+		$('#edit-product-' + productId).addClass('update-product');
+		$('#edit-product-' + productId).html('Save Update' + saveGlyphicon);
+
+		$('#delete-product-' + productId).removeClass('delete-product');
+		$('#delete-product-' + productId).addClass('cancel-product-update');
+		$('#delete-product-' + productId).html('Cancel Update' + cancelGlyphicon);
+	},
+
+	updateProduct: function(click) {
+		var productId = click.target.dataset.productId;
+		var query = new Parse.Query('Product');
+		var productName = $('#edit-product-' + productId).parents('tr').find('input')[0].value;
+		var productDescription = $('#edit-product-' + productId).parents('tr').find('input')[1].value;
+
+		query.get(productId, {
+			success: function(product) {
+				product.set('name', productName);
+				product.set('description', productDescription);
+
+				product.save({
+					success: function() {
+						sweetAlert('Success!', 'The product has been updated.', 'success');
+					},
+					error: function(error) {
+						sweetAlert('Oops!', 'The product was unable to be updated.', 'error');
+						console.log(error);
+					}
+				});
+			},
+			error: function(error) {
+				sweetAlert('Oops!', 'The product was unable to be updated.', 'error');
+				console.log(error);
+			}
+		});
+
+		$('.cancel-product-update#delete-product-' + productId).click();
+
+		$($('#edit-product-' + productId).parents('tr').children('td')[0]).html(productName);
+		$($('#edit-product-' + productId).parents('tr').children('td')[1]).html(productDescription);
+	},
+
+	resetProductButtons: function(click) {
+		var productId = click.target.dataset.productId;
+		var editGlyphicon = '&nbsp;&nbsp;<span class="glyphicon glyphicon-pencil"></span>';
+		var deleteGlyphicon = '&nbsp;&nbsp;<span class="glyphicon glyphicon-trash"></span>';
+		var productName = $('#edit-product-' + productId).parents('tr').find('input')[0].dataset.originalValue;
+		var productDescription = $('#edit-product-' + productId).parents('tr').find('input')[1].dataset.originalValue;
+
+		$($('#edit-product-' + productId).parents('tr').children('td')[0]).html(productName);
+		$($('#edit-product-' + productId).parents('tr').children('td')[1]).html(productDescription);
+
+		$('#edit-product-' + productId).removeClass('update-product');
+		$('#edit-product-' + productId).addClass('btn-info');
+		$('#edit-product-' + productId).addClass('edit-product');
+		$('#edit-product-' + productId).html('Edit Product' + editGlyphicon);
+
+		$('#delete-product-' + productId).addClass('delete-product');
+		$('#delete-product-' + productId).removeClass('cancel-product-update');
+		$('#delete-product-' + productId).html('Delete Product' + deleteGlyphicon);
 	}
 });
