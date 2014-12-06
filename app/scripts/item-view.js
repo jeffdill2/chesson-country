f60b09b38a40e0eea168bfbae4a2e4493dc850b0
Jeffrey Dill <jeffdill2@gmail.com>
2014-12-06 16:53:50 -0500

    Rebuild social sharing links

--- app/scripts/item-view.js
+++ app/scripts/item-view.js
@@ -18,6 +18,12 @@ var ItemView = Parse.View.extend({
 		var _this = this;
 		var query = new Parse.Query('Item');
 		var innerQuery = new Parse.Query('Item');
+		var rootURL = 'http://chessoncountry.com';
+		var facebookURL = '//www.facebook.com/sharer/sharer.php';
+		var twitterURL = '//twitter.com/share';
+		var googlePlusURL = '//plus.google.com/share';
+		var pinterestURL = '//pinterest.com/pin/create/button';
+		var twitterHandle = '@ChessonCountry';
 
 		query.include('product');
 
@@ -34,9 +40,13 @@ var ItemView = Parse.View.extend({
 				innerQuery.find({
 					success: function(relatedItems) {
 						_this.$el.html(_this.template({
-							item: 				item,
-							relatedItems: relatedItems.slice(0, 5),
-							itemsCount: 	relatedItems.length
+							item: 					item,
+							relatedItems: 	relatedItems.slice(0, 5),
+							itemsCount: 		relatedItems.length,
+							facebookLink: 	facebookURL + '?u=' + rootURL + '/%23items/' + item.id,
+							twitterLink: 		twitterURL + '?url=' + rootURL + '/%23items/' + item.id + '&text=' + item.attributes.name + '&via=' + twitterHandle,
+							googlePlusLink: googlePlusURL + '?url=' + rootURL + '/%23items/' + item.id,
+							pinterestLink: 	pinterestURL + '?url=' + rootURL + '/%23items/' + item.id + '&media=' + item.attributes.image._url + '&description=' + item.attributes.name + "  |  " + item.attributes.description
 						}));
 					},
 					error: function(error) {
